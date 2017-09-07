2680772733b21b985dc02c454c21a5b28b2c60c9
Zeming Li <zemingli777@gmail.com>
2017-09-07 23:40:19 +0800

    Add files via upload

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by lizeming on 2017-09-07.
+//  Copyright © 2017 lizeming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textViewer: UITextView!
+    @IBOutlet weak var tf: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
